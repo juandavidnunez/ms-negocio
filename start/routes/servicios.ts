c3d9e4f837bafd2b0fa8557390360bd7d19dad9a
juandavidnunez <juandavidnunez83@gmail.com>
2024-12-17 22:02:13 -0500

    juan

--- start/routes/servicios.ts
+++ start/routes/servicios.ts
@@ -2,7 +2,7 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.get('/servicios', 'ServiciosController.findAll')
-  Route.get('/servicios/:id', 'ServiciosController.show')
+  Route.get('/servicios/:id', 'ServiciosController.findById')
   Route.post('/servicios', 'ServiciosController.create')
   Route.put('/servicios/:id', 'ServiciosController.update')
   Route.delete('/servicios/:id', 'ServiciosController.delete')
